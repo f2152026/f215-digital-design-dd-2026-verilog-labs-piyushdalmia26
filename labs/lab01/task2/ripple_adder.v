6619b76289d13cd8c37b6552d0c5017ea360088f
PiyushDalmia26 <f20250503@pilani.bits-pilani.ac.in>
2026-08-28 16:47:46 +0000

    lab01 task2

--- labs/lab01/task2/ripple_adder.v
+++ labs/lab01/task2/ripple_adder.v
@@ -21,6 +21,7 @@ module ripple_adder(
 );
 
   wire c1, c2, c3;
+  
 
 FA_Gate FA1(.a(a[0]),.b(b[0]),.cin(cin),.sum(sum[0]),.cout(c1));
 FA_Gate FA2(.a(a[1]),.b(b[1]),.cin(c1),.sum(sum[1]),.cout(c2));
